1dff99597f190c78a4152979b139ea14dca334d5
Danil Ardi <danilardi13@gmail.com>
2024-07-26 16:32:13 +0700

    UPDATE

--- src/utils/client.js
+++ src/utils/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuthToken } from "./API/Auth";
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   authorization: `Bearer ${getAuthToken()}`,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,16 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
-  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
-
-  return defineConfig({
-    plugins: [react()],
-
-    server: {
-      proxy: {
-        '/api': {
-          target: process.env.VITE_BACKEND_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
-    },
-  });
-}
+export default defineConfig({
+  plugins: [react()],
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://www.sixty-rivers-sort-freely.a276.dcdg.xyz',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
+})
